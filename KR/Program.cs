1bbc8767ea62c041f998581329bdf9376264f94f
Байкалов Владислав Евгеньевич гр. ИС(ПРО)-31 <is12750@stud.KHPK.RU>
2022-04-12 13:51:52 +0700

    first commit

---
+++ KR/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace KR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+           User user = new User();
+            user.SGEmail = "test@gmail.";
+            Console.WriteLine(user.SGEmail);
+        }
+    }
+}
---
+++ KR/Shop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KR
+{
+    interface ISubjectObserverable
+    {
+        void Add(IpropertyObserver observer);
+        void Remove(IpropertyObserver observer);
+        void Notify(string message);
+    }
+    internal class Shop: ISubjectObserverable
+    {
+        private HashSet<IpropertyObserver> observers= new HashSet<IpropertyObserver> ();
+        public void Add(IEnumerable <IpropertyObserver> observer) 
+        {
+            foreach (var o in observer) Add(o); 
+        }
+
+        public void Add(IpropertyObserver observer) => observers.Add(observer);
+
+
+        public void Remove(IpropertyObserver observer) => observers.Remove(observer);
+        public void Notify(string message) {
+            foreach (var observer in observers)
+            {
+
+            }
+        }
+
+    }
+}
